89df145f5709764d1097aded7fd30d60f05bd434
lshiv080374 <lavanyashivram@gmail.com>
2021-06-03 20:56:41 -0400

    latest

--- projects/assignment_1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package assignment_1;
-
-public class HelloWorld {
-    final private static String statement = "Lavanya Shivram";
-    public static void main (String[] args)  {
-        System.out.println(statement);
-    }
-}
